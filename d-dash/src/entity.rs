cc22389c0a0d7284e5346bb4ac95d7daa206b485
buraksenyurt <burakselimsenyurt@gmail.com>
2025-10-05 14:45:20 +0300

    Implement directory entity representation: add Entity struct and update main logic to list files and directories with their types and sizes

---
+++ d-dash/src/entity.rs
@@ -0,0 +1,79 @@
+#[derive(Debug)]
+pub enum Type {
+    File,
+    Directory,
+}
+
+#[derive(Debug)]
+pub struct Entity {
+    pub name: String,
+    pub entity_type: Type,
+    pub size: u64,
+    pub extension: Option<String>,
+}
+
+impl Entity {
+    pub fn new(name: String, entity_type: Type, size: u64, extension: Option<String>) -> Self {
+        Self {
+            name,
+            entity_type,
+            size,
+            extension,
+        }
+    }
+}
+
+impl std::fmt::Display for Entity {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let entity_type = match self.entity_type {
+            Type::File => "File",
+            Type::Directory => "Dir",
+        };
+        let extension = self.extension.as_ref().map_or("", |ext| ext.as_str());
+        write!(
+            f,
+            "{:<25}\t{:<5}\t{:<10}\t{} bytes",
+            self.name,
+            entity_type,
+            match self.entity_type {
+                Type::File => get_extension_means(extension).unwrap_or("Unknown"),
+                Type::Directory => "",
+            },
+            self.size
+        )
+    }
+}
+
+fn get_extension_means(extension:&str)-> Option<&'static str> {
+    match extension {
+        "txt" => Some("Text File"),
+        "rs" => Some("Rust Source File"),
+        "py" => Some("Python Source File"),
+        "js" => Some("JavaScript File"),
+        "html" => Some("HTML File"),
+        "css" => Some("CSS File"),
+        "json" => Some("JSON File"),
+        "xml" => Some("XML File"),
+        "jpg" | "jpeg" => Some("JPEG Image"),
+        "png" => Some("PNG Image"),
+        "gif" => Some("GIF Image"),
+        "bmp" => Some("Bitmap Image"),
+        "mp3" => Some("MP3 Audio File"),
+        "wav" => Some("WAV Audio File"),
+        "mp4" => Some("MP4 Video File"),
+        "mkv" => Some("MKV Video File"),
+        "pdf" => Some("PDF Document"),
+        "doc" | "docx" => Some("Word Document"),
+        "xls" | "xlsx" => Some("Excel Spreadsheet"),
+        "ppt" | "pptx" => Some("PowerPoint Presentation"),
+        "md" => Some("Markdown File"),
+        "zip" => Some("ZIP Archive"),
+        "toml" => Some("TOML File"),
+        "yaml" | "yml" => Some("YAML File"),
+        "csv" => Some("CSV File"),
+        "exe" => Some("Windows Executable"),
+        "lock" => Some("Lock File"),
+        "bin" => Some("Binary File"),
+        _ => None,
+    }
+}
--- d-dash/src/main.rs
+++ d-dash/src/main.rs
@@ -1,7 +1,12 @@
 use std::{fs, io};
+mod entity;
+mod utility;
+use entity::{Entity, Type};
 
 use colorized::{Color, Colors};
 fn main() -> io::Result<()> {
+    utility::clear_screen();
+
     let path = std::env::args().nth(1).unwrap_or_else(|| ".".to_string());
     println!(
         "{}",
@@ -22,6 +27,7 @@ fn main() -> io::Result<()> {
         return Ok(());
     }
 
+    let mut entities = Vec::new();
     let entries = fs::read_dir(&path)?;
     for entry in entries {
         match entry {
@@ -29,27 +35,17 @@ fn main() -> io::Result<()> {
                 let file_type = entry.file_type()?;
                 let file_name = entry.file_name().into_string().unwrap_or_default();
                 if file_type.is_dir() {
-                    println!(
-                        "{}",
-                        format!("Dir:  {}", file_name)
-                            .as_str()
-                            .color(Colors::BrightYellowFg)
-                    );
+                    let entity = Entity::new(file_name, Type::Directory, 0, None);
+                    entities.push(entity);
                 } else if file_type.is_file() {
                     let metadata = fs::metadata(entry.path())?;
-                    println!(
-                        "{}",
-                        format!("File: {}, {} bytes", file_name, metadata.len())
-                            .as_str()
-                            .color(Colors::BrightGreenFg)
-                    );
-                } else {
-                    println!(
-                        "{}",
-                        format!("Other: {}", file_name)
-                            .as_str()
-                            .color(Colors::BrightYellowFg)
-                    );
+                    let extension = entry
+                        .path()
+                        .extension()
+                        .and_then(|ext| ext.to_str())
+                        .map(|s| s.to_string());
+                    let entity = Entity::new(file_name, Type::File, metadata.len(), extension);
+                    entities.push(entity);
                 }
             }
             Err(e) => eprintln!(
@@ -59,6 +55,23 @@ fn main() -> io::Result<()> {
                     .color(Colors::BrightRedFg)
             ),
         }
+    }
+
+    println!(
+        "{}",
+        format!(
+            "\nFound {} entities in directory '{}':",
+            entities.len(),
+            path
+        )
+        .as_str()
+        .color(Colors::BrightCyanFg)
+    );
+    for entity in entities {
+        println!(
+            "{}",
+            format!("{}", entity).as_str().color(Colors::BrightYellowFg)
+        );
     }
 
     Ok(())
